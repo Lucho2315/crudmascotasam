5279e0a21b3f1e11f721e479a43dd398bd97133f
LUIS ENRIQUE MARQUEZ <luismarquez98@hotmail.com>
2024-07-28 16:26:08 -0500

    Correccion creacion archivos

---
+++ router/rutasweb.js
@@ -0,0 +1,11 @@
+router.get('/', (req, res) => {					
+    				
+    res.render('index', {titulo: "Bienvenido a Node.js con Express y con EJS"});					
+})					
+                    
+router.get("/servicios", (req, res)=>{					
+                    
+ res.render('servicios', {titulo: "Nuestros Servicios Dinámicos"});					
+ });
+ 
+ module.exports= router;
