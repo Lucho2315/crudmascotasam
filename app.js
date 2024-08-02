b79a9ba8b9d63afc01ab315c650b91df4b2792eb
LUIS ENRIQUE MARQUEZ <luismarquez98@hotmail.com>
2024-08-02 08:03:07 -0500

    Conexion a la BD Mongodb

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')					
 const app = express()					
 const port = 3000					
-					
+
+/* conexion a la base de datos mongodb cloud */
+const mongoose = require('mongoose');
+const usuario = 'campus_full_stack';
+const password = 'nfrRTMasvCl4ZeCh';
+const dbName = 'veterinaria';
+
+const uri = `mongodb+srv://${usuario}:${password}@cluster0.gp402lu.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.connect(uri)
+  .then(()=> console.log('conectado a MongoDB')) 
+  .catch(e=> console.log('error de conexión a MongoDB', e))
 /* establecemos el motor de plantillas */					
 app.set('view engine', 'ejs');					
 app.set('views', __dirname + '/views');					
