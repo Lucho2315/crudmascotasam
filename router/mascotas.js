eaab79dadb99ccb3e4826590f004ede0997de4dc
LUIS ENRIQUE MARQUEZ <luismarquez98@hotmail.com>
2024-08-04 18:49:45 -0500

    Creacion Crud

--- router/mascotas.js
+++ router/mascotas.js
@@ -12,8 +12,21 @@ router.get("/", async (req, res)=>{
         console.log(error)
     }
 });
+router.get("/crear", (req, res)=>{
+    res.render('crear');
+});
 
-
+/* router para recibir datos del formulario crear */
+router.post("/", async (req, res)=>{
+    const body = req.body;
+    // console.log(body);
+    try {
+        await Mascota.create(body)
+        res.redirect('/mascotas')
+    } catch (error) {
+        console.log('error: ', error)
+    }
+});
 
 
 module.exports = router;
